13ea8f965d9e7441510c157667bf470344e50d79
Kasper Welbers <kasperwelbers@gmail.com>
2022-04-08 10:40:18 +0200

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name="amcat4annotator",
-    version="0.16",
+    version="0.17",
     description="Annotator Backend API for AmCAT4 Text Analysis",
     author="Wouter van Atteveldt, Farzam Fanitabasi",
     author_email="wouter@vanatteveldt.com",

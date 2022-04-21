6af907ab6bc9cd8c867c02f2ea3cafe4be1aff89
Kasper Welbers <kasperwelbers@gmail.com>
2022-04-21 18:23:30 +0200

    back to old version system

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name="amcat4annotator",
-    version="0.11.3",
+    version="0.20",
     description="Annotator Backend API for AmCAT4 Text Analysis",
     author="Wouter van Atteveldt, Farzam Fanitabasi",
     author_email="wouter@vanatteveldt.com",

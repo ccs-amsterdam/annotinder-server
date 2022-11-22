f0a135cb3ba7d2769445cabee5a2885b09de3391
Kasper Welbers <kasperwelbers@gmail.com>
2022-11-22 16:49:13 +0100

    is this the issue>?

--- annotinder/api/__init__.py
+++ annotinder/api/__init__.py
@@ -43,7 +43,7 @@ app.include_router(app_annotator_guest)
 app.add_middleware(
   CORSMiddleware,
   allow_origins=["*"],
-  allow_credentials=True,
+  allow_credentials=False,
   allow_methods=["*"],
   allow_headers=["*"],
 )
